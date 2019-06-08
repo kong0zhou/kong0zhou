791380712917547a640486a5e9e63bbbb7a36afa
kongzhou <715536017@qq.com>
2019-06-08 11:41:12 +0800

    15

--- src/main.go
+++ src/main.go
@@ -1,27 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os/exec"
 )
 
 func main() {
 	// os.Chdir("../shell")
-	params := make([]string, 1)
-	params[0] = "php.sh"
+	params := make([]string, 2)
+	params[0] = "-c"
+	params[1] = "php.sh"
 	_ = execCommand("/bin/bash", params)
 	fmt.Println("21564564")
 }
 
 func execCommand(commandName string, params []string) bool {
-	cmd := exec.Command(commandName, "-c", "php.sh")
+	cmd := exec.Command(commandName, params...)
+
 	//显示运行的命令
-	// fmt.Println(cmd.Args)
-	out, err := cmd.Output()
+	fmt.Println(cmd.Args)
+
+	stdout, err := cmd.StdoutPipe() //接收命令在控制行里输出的数据（字符串）
 	if err != nil {
 		fmt.Println(err)
-		fmt.Println("45646416")
+		return false
 	}
-	fmt.Println(string(out))
+
+	cmd.Start()
+
+	reader := bufio.NewReader(stdout)
+
+	//实时循环读取输出流中的一行内容(即打印到控制的数据)
+	for {
+		line, err2 := reader.ReadString('\n')
+		if err2 != nil || io.EOF == err2 {
+			break
+		}
+		fmt.Println("hey:", line)
+	}
+
+	cmd.Wait()
 	return true
 }
