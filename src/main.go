2070fe67ec64d2e008231ebcc16d478f00fdbfe7
kongzhou <715536017@qq.com>
2019-06-08 10:48:32 +0800

    10

--- src/main.go
+++ src/main.go
@@ -7,18 +7,15 @@ import (
 
 func main() {
 	// os.Chdir("../shell")
-	params := make([]string, 2)
-	params[0] = "-c"
-	params[1] = "php.sh"
+	params := make([]string, 1)
+	params[0] = "php.sh"
 	_ = execCommand("/bin/bash", params)
 }
 
 func execCommand(commandName string, params []string) bool {
 	cmd := exec.Command(commandName, params...)
-
 	//显示运行的命令
 	// fmt.Println(cmd.Args)
-
 	out, _ := cmd.Output()
 	fmt.Println(string(out))
 	return true
