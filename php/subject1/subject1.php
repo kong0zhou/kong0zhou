b9618d6d3c39320bfe9c8d73458a69704568cebe
kongzhou <715536017@qq.com>
2019-07-08 17:16:02 +0800

    123

--- php/subject1/subject1.php
+++ php/subject1/subject1.php
@@ -1,3 +1,34 @@
 <?php 
-	echo "2131654";
+//设置最大执行时间
+set_time_limit(0);
+function getHtml($url){
+  // 1. 初始化
+   $ch = curl_init();
+   // 2. 设置选项，包括URL
+
+   //设置需要获取的url地址
+   curl_setopt($ch,CURLOPT_URL,$url);
+   //设置是否以字符串形式返回
+   curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+   //
+   curl_setopt($ch,CURLOPT_HEADER,0);
+   // 3. 执行并获取HTML文档内容
+   $output = curl_exec($ch);
+   if($output === FALSE ){
+    $output = '';
+   }
+   // 4. 释放curl句柄
+   curl_close($ch);
+   return $output;
+}
+function getPageData($url){
+  // 获取整个网页内容
+  $html = getHtml($url);
+  preg_match_all("/<p.*>.*<\/p>/",$html,$result_array);
+  return $result_array;
+}
+
+$arr = getPageData("https://blog.csdn.net/YDTG1993/article/details/83861629");
+print_r($arr)
+
 ?>
