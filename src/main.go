472072fd28a95a01c8201cd9c6a5ceede58b48cc
kong0zhou <47652471+kong0zhou@users.noreply.github.com>
2019-07-21 13:37:48 +0800

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,19 +1,18 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"os/exec"
+	"net/http"
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "hello world")
+	fmt.Fprintln(w, "hello world")
 }
 
-func main(){
+func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", IndexHandler)
-	err = http.ListenAndServe(":8083", mux)
+	err := http.ListenAndServe(":8083", mux)
 	if err != nil {
 		fmt.Println("启动失败")
 	}
