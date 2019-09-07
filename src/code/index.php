74fcb50ae13bf4d0be51ecb419e29418b17a2102
zhouzeming <715536017@qq.com>
2019-09-07 17:37:47 +0800

    z

--- src/code/index.php
+++
@@ -1,4 +0,0 @@
-<?php ;
-echo "这是输出shdfksdkjfhskjdhfjksdhkjfhsdjkh\n";
-echo "dsfsdf";
-?> 
--- src/php.sh
+++
@@ -1,8 +0,0 @@
-PHP=`pwd`
-docker run --name php \
-	--rm \
-	-it \
-	-v $PHP/code:/code \
-	php \
-	php /code/index.php
-	时空
